bbdce5ed442164aaddd464306a24b1325b690391
ZenkichiTerasaki <zenoha77@outlook.jp>
2026-06-25 18:17:00 +0900

    feat: integrate NestJS Prisma and PostgreSQL

---
+++ backend/src/project/project.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { ProjectService } from './project.service';
+
+@Controller('projects')
+export class ProjectController {
+  constructor(
+    private readonly projectService: ProjectService,
+  ) {}
+
+  @Get()
+  findAll() {
+    return this.projectService.findAll();
+  }
+}
